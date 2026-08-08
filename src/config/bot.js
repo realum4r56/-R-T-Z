d90356c86b5daa0818bb0f1f6f43139da4facc3f
realum4r56 <realum4r56@gmail.com>
2026-08-08 21:29:08 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 0,               // Custom
+        type: 5,               // Custom
       },
     ],
   },
