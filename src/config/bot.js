0cef9b76089012127bc4f68cdce350a0a28231c5
realum4r56 <realum4r56@gmail.com>
2026-08-13 20:43:57 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Baan raha hai !", // required by Discord API, not shown in the client
-        state: "Baan raha hai !",     // this is what people actually see
+        name: "Watching...", // required by Discord API, not shown in the client
+        state: "Watching...",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
